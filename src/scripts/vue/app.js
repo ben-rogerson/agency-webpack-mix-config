3405938a4d72954d8b6307e293b68d2e92d80ee8
Ben Rogerson <info@benrogerson.com.au>
2019-06-30 08:38:26 +0930

    turn off vue tip

--- src/scripts/vue/app.js
+++ src/scripts/vue/app.js
@@ -1,6 +1,8 @@
 const App = async () => {
     const Vue = await import(/* webpackChunkName: "vue-import" */ "vue")
 
+    Vue.default.config.productionTip = false;
+
     new Vue.default({
         el: "#app",
         delimiters: ["${", "}"],
